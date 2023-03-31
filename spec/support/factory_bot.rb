160a84bd1ed7cbf09ea8b303f988851611185bda
RoyJumah <royjumah17@gmail.com>
2023-03-31 19:34:08 +0300

    fix linting errors

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :post do
     # attributes for the post factory
-    title { "My awesome post" }
-    body { "This is the content of my post" }
+    title { 'My awesome post' }
+    body { 'This is the content of my post' }
     user # association with the user factory
   end
 end
