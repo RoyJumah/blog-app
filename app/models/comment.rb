6412ec49a103fd816e6093178b4aa7d9848b348c
RoyJumah <royjumah17@gmail.com>
2023-03-31 19:45:04 +0300

    fix tests linting and test errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,14 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  after_create :update_post_comments_counter
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  validates :text, presence: true
+
+  after_create :update_comments_counter
 
   private
 
-  def update_post_comments_counter
-    post.update(comments_counter: post.comments.count)
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
