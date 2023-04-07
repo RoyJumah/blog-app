caf9d4faf15a8b5d19975919a536211ed855531f
Roy Jumah <royjumah17@gmail.com>
2023-04-07 13:00:38 +0300

    Add relevant forms and create methods to add comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  post 'likes/:post_id', to: 'likes#create'
+  post 'comments/:post_id', to: 'comments#create'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
