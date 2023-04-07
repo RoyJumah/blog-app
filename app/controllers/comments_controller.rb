618a285d994a352f835bf5f46f96f6a7e2567015
Roy Jumah <royjumah17@gmail.com>
2023-04-07 16:46:51 +0300

    make suggested changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
   def create
     @comment = current_user.comments.new(comment_params)
     @post = Post.find(params[:post_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
-  post 'likes/:post_id', to: 'likes#create'
-  post 'comments/:post_id', to: 'comments#create'
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
+    end
   end
+
+  post 'likes/:post_id', to: 'likes#create'
+  post 'comments/:post_id', to: 'comments#create'
 end
