a31e46939efacfc32d3196b2a80650647ca76cbb
Roy Jumah <royjumah17@gmail.com>
2023-04-11 20:07:34 +0300

    add tests for pageshow and pageindex

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @user = User.includes(:posts, :comments).find(params[:user_id])
+    @pagy, @posts = pagy(@user.posts, items: 10)
   end
 
   def show
