ab82d8c2206de34846bdebd2dcd70a9c5b76e649
Roy Jumah <royjumah17@gmail.com>
2023-04-13 12:41:24 +0300

    make suggested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :confirmable
   attribute :posts_counter, :integer, default: 0
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  # validates :name, presence: true
+  validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
