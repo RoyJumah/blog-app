e93610c6b5be83e1c824c778eca11ee10fc004d5
Roy Jumah <royjumah17@gmail.com>
2023-04-16 10:20:00 +0300

    Add API endpoint to create, list all posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # get 'home/header'
+  
+  # Move these routes inside the `api` namespace if they are part of your API
   devise_for :users
-  root to: 'users#index'
-
+   root to: 'users#index'
+  
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
   delete 'posts/:id', to: 'posts#destroy', as: :delete_post
   delete 'posts/:post_id/comments/:id', to: 'comments#destroy', as: :delete_comment
-
-
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
@@ -16,7 +15,26 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
-
+  
   post 'likes/:post_id', to: 'likes#create'
   post 'comments/:post_id', to: 'comments#create'
-end
+
+  namespace :api do
+    namespace :v1 do
+      # List all posts for a user
+
+     resources :users, only: [:index, :show] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    
+    end
+
+      # Add a comment to a post
+      # post '/posts/:post_id/comments', to: 'posts#create_comment'
+  end 
+ 
+  end
+
+end 
+
